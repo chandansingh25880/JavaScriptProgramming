dfa0c0b511a05d6f6e4a18660ed508a03be455e8
Chandan Singh Pawar <79848243+chandansingh25880@users.noreply.github.com>
2021-05-08 06:16:32 +0530

    Added: To flip the coin Head or Tail

---
+++ Selectionifelse/flipCoin.js
@@ -0,0 +1,7 @@
+let flip=(Math.floor(Math.random() * 2 + 1));
+let head=2;
+if ( flip == head ){
+console.log("coin is flipped and HEADS it is");
+} else {
+console.log("coin is flipped and TAILS it is");
+}
