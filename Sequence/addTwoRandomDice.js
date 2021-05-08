90485f2a942252662610a0fd7c5a7f4eada243bc
Chandan Singh Pawar <79848243+chandansingh25880@users.noreply.github.com>
2021-05-08 05:45:10 +0530

    Added: Adding of two RandomDice number

---
+++ Sequence/addTwoRandomDice.js
@@ -0,0 +1,6 @@
+let a = (Math.floor(Math.random() * 999 + 1));
+console.log("dice1 " + a);
+let b = (Math.floor(Math.random() * 99 + 1));
+console.log("dice2 " +  b);
+let c = a + b;
+console.log("Add of RandomDice " + c);
